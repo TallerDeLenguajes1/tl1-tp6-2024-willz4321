c5293b40efbaab0def0c375be24a07bc1f41e01b
willz4321 <willz4321@gmail.com>
2024-05-26 22:34:10 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- int a;
- int b;
- a=10;
- b=a;
- Console.WriteLine("valor de a:"+a);
- Console.WriteLine("valor de b:"+b);
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Por favor, ingrese un numero:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number > 0)
+            {
+                string reversedNumber = ReverseNumber(number);
+                Console.WriteLine($"El número invertido es: {reversedNumber}");
+            }
+            else
+            {
+                Console.WriteLine("El número ingresado no es mayor a 0. No se puede invertir.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El texto ingresado no es un número válido.");
+        }
+    }
+
+    static string ReverseNumber(int number)
+    {
+        char[] numberArray = number.ToString().ToCharArray();
+        Array.Reverse(numberArray);
+        return new string(numberArray);
+    }
+}
